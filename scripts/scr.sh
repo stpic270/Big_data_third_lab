46ccbd09ac051f4b50e9a0f7265d7ced795040f3
Your Name <kasai.stepa@gmail.com>
2023-09-15 21:04:59 +0500

    Memory changes

--- scripts/scr.sh
+++ scripts/scr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+python src/preprocess.py && python src/train.py -m BNB && python src/predict.py -m BNB -t func && coverage run src/unit_tests/test_preprocess.py && coverage run -a src/unit_tests/test_training.py && coverage report -m
 ansible-vault encrypt --vault-password-file my_password.txt test/cassandra_config.txt 
 echo "Sleeping had started"
 sleep infinity
-echo "Sleeping has ended"
+
